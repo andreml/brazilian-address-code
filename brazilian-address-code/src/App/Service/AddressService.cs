f0514333db5c99df7c81888071e3b6e507600993
andremunizdelima <andremunizdelima@hotmail.com>
2023-01-21 19:02:53 -0300

    Include infraestructure code

---
+++ brazilian-address-code/src/App/Service/AddressService.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace App.Service
+{
+    public class AddressService
+    {
+        HttpClient _;
+        public AddressService()
+        {
+            _ = new HttpClient();
+        }
+
+        public async Task<AddressResponse> GetAddressByZipCode(string code)
+        {
+            var url = $"https://viacep.com.br/ws/{code}/json/".Replace("-","");
+            var response = await _.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var validResult = response.Content.ReadAsStringAsync().Result;
+
+                if (validResult.Contains("true")) return null;
+                
+                var result = JsonConvert.DeserializeObject<AddressResponse>(validResult);
+                return result;
+            }
+            else
+            {
+                throw new Exception("Error");
+            }
+        }
+    }
+}
---
+++ brazilian-address-code/src/Domain/Entities/Address.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Domain.Entities
+{
+    public class Address
+    {
+        public string zipCode { get; set; }
+        public bool IsValid()
+        {
+            if (string.IsNullOrEmpty(zipCode)) { return false; }
+            if (Regex.Matches(zipCode.Replace("-", ""), @"[a-zA-Z]").Count > 0) return false;
+            if (zipCode.Replace("-", "").Length != 8) return false;
+
+            return true;
+        }
+    }
+}
