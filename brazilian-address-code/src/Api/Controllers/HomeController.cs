29d853c28c9e2bc8569e3c057dcfa9f8a3b7b70f
andremunizdelima <andremunizdelima@hotmail.com>
2023-03-23 15:17:06 -0300

    Include controller

---
+++ brazilian-address-code/src/Api/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using App.Service;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace brazilian_address_code.Controllers
+{
+    [ApiController]
+    [Route("Address")]
+    public class HomeController : Controller
+    {
+        [HttpGet("{zipCode}")]
+        public async Task<IActionResult> Index(string zipCode)
+        {
+            AddressService _service = new AddressService();
+            var result = await _service.GetAddressByZipCode(zipCode);
+
+            return StatusCode(200, result);
+        }
+    }
+}
